22fcb3adceab77ca4a830d502029625cfe3e134a
Asa <>
2025-09-10 14:53:16 +0800

    Update Spotify module and scripts

--- js/spotify-json.js
+++ js/spotify-json.js
@@ -1,9 +1,10 @@
-console.log(`spotify-json-2023.06.13`);
+console.log(`spotify-json-2025.06.20`);
 let url = $request.url;
-// console.log(`og url:${url}`);
+if (url.includes("com:443")) {
+  url = url.replace(/com:443/, "com");
+}
 if (url.includes("platform=iphone")) {
   url = url.replace(/platform=iphone/, "platform=ipad");
-  // console.log(`replace platform:${url}`);
 } else {
   console.log("nothing");
 }
